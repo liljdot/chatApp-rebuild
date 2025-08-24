fe0f2e52713678ba93e8c2fd8be6b4251d2a1ddd
liljdot <jehuoki@gmail.com>
2025-08-24 21:02:22 +0100

    feat:createUniqueProfilepic function

--- server/src/util/auth.ts
+++ server/src/util/auth.ts
@@ -4,3 +4,7 @@ export const hashNewPassword = (password: string) => {
     return bcrypt.genSalt(10)
     .then(salt => bcrypt.hash(password, salt))
 }
+
+export const createUniqueProfilepic: (username: string, gender: "male" | "female") => string = (username, gender) => {
+    return `https://avatar.iran.liara.run/public/${gender == "male" ? "boy" : "girl"}?username=${username}`
+}
