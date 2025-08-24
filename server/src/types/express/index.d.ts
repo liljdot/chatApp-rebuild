d3e0b5d67fbc32ae523ce586e5b90c7c402abf88
liljdot <jehuoki@gmail.com>
2025-08-24 19:59:25 +0100

    feat: enhance Response interface to include custom json method for standardized responses

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -4,4 +4,20 @@ declare module "express" {
     interface Request {
         user?: any
     }
+
+    interface Response {
+        json: (obj: {
+        status: 200 | 201
+        message: string
+        data: any
+    }
+        | {
+            status: 204
+        }
+        | {
+            status: 400 | 401 | 403 | 404 | 429 | 500
+            message: string
+            error: any
+        }) => any
+    }
 }
