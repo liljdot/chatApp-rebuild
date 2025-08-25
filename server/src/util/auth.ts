acec492a0437ecce4953ecf67063150bd2ff50f6
liljdot <jehuoki@gmail.com>
2025-08-25 19:44:35 +0100

    feat: enhance auth utility functions with token generation

--- server/src/util/auth.ts
+++ server/src/util/auth.ts
@@ -1,8 +1,27 @@
+import { config } from "dotenv"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+config()
 
 export const hashNewPassword = (password: string) => {
     return bcrypt.genSalt(10)
-    .then(salt => bcrypt.hash(password, salt))
+        .then(salt => bcrypt.hash(password, salt))
+}
+
+export const generateToken = (id: string) => {
+    const { JWT_SECRET } = process.env
+
+    const token = jwt.sign(
+        {
+            userId: id
+        },
+        JWT_SECRET!,
+        {
+            expiresIn: "10m"
+        })
+
+    return token
 }
 
 export const createUniqueProfilepic: (username: string, gender: "male" | "female") => string = (username, gender) => {
