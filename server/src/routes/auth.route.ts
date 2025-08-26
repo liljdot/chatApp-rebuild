27681af00d72f238ebbe162b78dcb38e9e80269f
liljdot <jehuoki@gmail.com>
2025-08-26 22:16:10 +0100

    import and use logout controller in authroutes

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import express from "express"
-import { postLogin, postSignup } from "../controllers/auth.controller.js"
+import { postLogin, postLogout, postSignup } from "../controllers/auth.controller.js"
 
 const authRoutes = express()
 
 authRoutes.post("/login", postLogin)
 
 authRoutes.post("/signup", postSignup)
+
+authRoutes.post("/logout", postLogout)
 
 export default authRoutes
