fa0980a552646e4f6191f5dab73a40c783e3cb80
liljdot <jehuoki@gmail.com>
2025-08-27 21:09:11 +0100

    import and use cookieparser middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+
 import authRoutes from "./routes/auth.route.js"
 
 const app = express()
 
 app.use(express.json()) // Middleware to parse JSON bodies
+app.use(cookieParser()) // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes)
 
