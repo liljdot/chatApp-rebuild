d43c02e2969831d1ef7fe06f3dc8832b2e9ea342
liljdot <jehuoki@gmail.com>
2025-08-27 20:56:27 +0100

    implement cleanUser tyoe in express req object

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,8 +1,9 @@
 import "express"
+import { CleanUser } from "../index.ts"
 
 declare module "express" {
     interface Request {
-        user?: any
+        user?: CleanUser
         error?: {
             status: 400 | 401 | 403 | 404 | 429 | 500
             message: string
