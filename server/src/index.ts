73b366c84a1ce88ef13c5f0edf948db3a37c3765
liljdot <jehuoki@gmail.com>
2025-08-28 21:37:36 +0100

    use message routes in app router

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json()) // Middleware to parse JSON bodies
 app.use(cookieParser()) // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
