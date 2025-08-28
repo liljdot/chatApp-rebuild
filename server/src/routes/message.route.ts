9688bf833600a01f112082b138eb5920ac561e80
liljdot <jehuoki@gmail.com>
2025-08-28 21:42:30 +0100

    create send message endpoint

--- server/src/routes/message.route.ts
+++ server/src/routes/message.route.ts
@@ -1,5 +1,8 @@
 import express from "express"
+import requireAuth from "../middleware/requireAuth.js";
 
 const messageRoutes = express()
+
+messageRoutes.post("/send/:recipientId", requireAuth, () => {})
 
 export default messageRoutes;
