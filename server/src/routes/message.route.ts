94a46b9c69010e473387471d9f2e7ac127291b5f
liljdot <jehuoki@gmail.com>
2025-08-29 20:06:12 +0100

    import and use postSendmessage controller in messagesroute

--- server/src/routes/message.route.ts
+++ server/src/routes/message.route.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import requireAuth from "../middleware/requireAuth.js";
+import { postSendMessage } from "../controllers/message.controller.js";
 
 const messageRoutes = express()
 
-messageRoutes.post("/send/:recipientId", requireAuth, () => {})
+messageRoutes.post("/send/:recipientId", requireAuth, postSendMessage)
 
 export default messageRoutes;
