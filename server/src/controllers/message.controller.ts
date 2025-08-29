1e7ff5efeb515e1f9bc113f7ceb2982383bc81eb
liljdot <jehuoki@gmail.com>
2025-08-29 20:05:11 +0100

    write logic for postSendMessage request

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -1,8 +1,101 @@
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
 
 const postSendMessage = (req: Request, res: Response) => {
-    const { recipientId } = req.params
-    console.log(recipientId)
+    // must be used with requireAuth middleware which will either set req.user or req.error
+
+    const { params: { recipientId }, body: { message }, error, user: sender } = req
+
+    if (error) {
+        console.log("auth error:", error) // because requireAuth runs first, any error it sets will be related to auth
+
+        return res.status(error.status || 500).json({
+            status: error.status || 500,
+            message: error.message || "Something went wrong",
+            error: error.error || "Internal server error"
+        })
+    }
+
+    if (!message) {
+        {
+            return res.status(400).json({
+                status: 400,
+                message: "Please provide a message",
+                error: "Missing message"
+            })
+        }
+    }
+
+    if (!sender) {
+        return res.status(401).json({
+            status: 401,
+            message: "You must be logged in to send messages",
+            error: "Unauthorized"
+        })
+    }
+
+    if (!recipientId) {
+        return res.status(400).json({
+            status: 400,
+            message: "Please provide a recipientId",
+            error: "Missing recipientId"
+        })
+    }
+
+    // check if conversation between sender and recipient already exists
+    prisma.conversation.findFirst({
+        where: {
+            AND: [
+                {
+                    User: { some: { id: sender.id } }
+                },
+                {
+                    User: { some: { id: recipientId } }
+                }
+            ]
+        }
+    })
+        .then(conversation => conversation
+            ? conversation
+            : prisma.conversation.create({
+                data: {
+                    participantIds: [sender.id, recipientId], // add id's to participant id's field
+                    User: {
+                        connect: [
+                            { id: sender.id },
+                            { id: recipientId }
+                        ] // connnect users to conversations (many to many) table relation. must be done with many to many relations
+                    }
+                }
+            })
+        )
+        .then(conversation => prisma.$transaction([
+            prisma.message.create({
+                data: {
+                    conversationId: conversation.id,
+                    senderId: sender.id,
+                    body: message
+                }
+            }), // connect message to conversation (many to one) table relation is not done here because it is done automatically via conversationId foreign key
+            prisma.conversation.update({
+                where: { id: conversation.id },
+                data: { updatedAt: new Date() } // update conversation's updatedAt field to current time
+            })
+        ]))
+        .then(([message]) => res.status(201).json({
+            status: 201,
+            message: "Message sent successfully",
+            data: message
+        }))
+        .catch(err => {
+            console.log("postSendMessage error:", err)
+
+            return res.status(500).json({
+                status: 500,
+                message: "Something went wrong",
+                error: "Internal server error"
+            })
+        })
 }
 
 export {
