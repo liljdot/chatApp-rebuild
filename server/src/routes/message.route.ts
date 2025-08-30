9b6a5e7f95f8ce917b3f6973e3d7e66ba873304c
liljdot <jehuoki@gmail.com>
2025-08-30 20:11:56 +0100

    import and use getConversations controller in messages route

--- server/src/routes/message.route.ts
+++ server/src/routes/message.route.ts
@@ -1,10 +1,10 @@
 import express from "express"
 import requireAuth from "../middleware/requireAuth.js";
-import { getConversationMessages, postSendMessage } from "../controllers/message.controller.js";
+import { getConversationMessages, getConversations, postSendMessage } from "../controllers/message.controller.js";
 
 const messageRoutes = express()
 
-messageRoutes.get("/conversations", requireAuth, () => {})
+messageRoutes.get("/conversations", requireAuth, getConversations)
 messageRoutes.get("/:conversationId", requireAuth, getConversationMessages)
 messageRoutes.post("/send/:recipientId", requireAuth, postSendMessage)
 
