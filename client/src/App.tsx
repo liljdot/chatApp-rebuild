b60e602b5a9a02623c419843376ca047bb03e91b
liljdot <jehuoki@gmail.com>
2025-08-31 19:22:24 +0100

    setup react router and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-      
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Routes>4
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/signup" element={<h1>Signup</h1>} />
+        </Routes>
+      </div>
     </>
   )
 }
