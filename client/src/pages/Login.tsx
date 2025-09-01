fa19f262712a8b9f404f9b391bcfbdcbb8af4f33
liljdot <jehuoki@gmail.com>
2025-09-01 19:48:50 +0100

    fill out login page ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
     return (
         <>
@@ -7,6 +9,42 @@ const Login = () => {
                         Login
                         <span className="text-blue-500"> ChatApp</span>
                     </h1>
+
+                    <form>
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-base label-text">Username</span>
+                            </label>
+                            <input type="text"
+                                placeholder="Enter username"
+                                className="w-full input input-bordered h-10"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-base label-text">Password</span>
+                            </label>
+                            <input type="password"
+                                placeholder="Enter password"
+                                className="w-full input input-bordered h-10"
+                            />
+                        </div>
+
+                        <Link
+                            to="/signup"
+                            className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block">
+                            {"Don't have an account?"}
+                        </Link>
+
+                        <div>
+                            <button
+                            type="submit"
+                            className="btn btn-block btn-sm mt-2 bg-blue-600">
+                            Login
+                        </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
