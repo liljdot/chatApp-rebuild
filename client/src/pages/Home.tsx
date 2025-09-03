7ab03136d4089fcfdf0ee50192a94ff947674ab8
liljdot <jehuoki@gmail.com>
2025-09-03 20:30:03 +0100

    add sidebar to home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import Sidebar from "@/components/home/Sidebar"
+
 const Home = () => {
   return (
     <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/0 bg-clip-padding backdrop-filter backdrop-blur-lg">
-      Home
+      <Sidebar />
     </div>
   )
 }
