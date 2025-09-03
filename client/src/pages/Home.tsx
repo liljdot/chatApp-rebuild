92aa315dfa5bd28365fe859e4a64d31cc5dc841e
liljdot <jehuoki@gmail.com>
2025-09-03 20:00:35 +0100

    setup homepage bg

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>
+    <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/0 bg-clip-padding backdrop-filter backdrop-blur-lg">
       Home
     </div>
   )
