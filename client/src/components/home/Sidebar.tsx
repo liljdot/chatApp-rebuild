d3cb792a9ce43f2194a0de1bea161f6fa6bef7c8
liljdot <jehuoki@gmail.com>
2025-09-05 20:05:28 +0100

    add divider in sidebar for better layout

--- client/src/components/home/Sidebar.tsx
+++ client/src/components/home/Sidebar.tsx
@@ -8,8 +8,8 @@ const Sidebar = () => {
     return (
         <div className={`border-r border-slate-500 p-4 flex flex-col ${conversationFieldOpen ? "max-w-0 px-0 overflow-y-hidden" : "max-w-screen"} md:p-4 md:max-w-100 transition-width duration-500 ease-in-out`}>
             <ConversationSearch />
+            <div className="divider px-3"></div>
             <ConversationsList conversations={[]} />
-
             <LogoutButton />
         </div>
     )
