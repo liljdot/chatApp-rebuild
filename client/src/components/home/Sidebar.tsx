e5fb8f397ebdfc90af8d9041a514b04bf2cbc77f
liljdot <jehuoki@gmail.com>
2025-09-05 20:02:54 +0100

    add logout button to sidebar

--- client/src/components/home/Sidebar.tsx
+++ client/src/components/home/Sidebar.tsx
@@ -1,3 +1,4 @@
+import LogoutButton from "@/features/auth/components/LogoutButton"
 import ConversationSearch from "@/features/conversation/components/ConversationSearch"
 import ConversationsList from "@/features/conversation/components/ConversationsList"
 
@@ -8,6 +9,8 @@ const Sidebar = () => {
         <div className={`border-r border-slate-500 p-4 flex flex-col ${conversationFieldOpen ? "max-w-0 px-0 overflow-y-hidden" : "max-w-screen"} md:p-4 md:max-w-100 transition-width duration-500 ease-in-out`}>
             <ConversationSearch />
             <ConversationsList conversations={[]} />
+
+            <LogoutButton />
         </div>
     )
 }
