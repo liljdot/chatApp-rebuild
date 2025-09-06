9acb91d7e8ff35000aaaf89d0118c7dd8dcf279e
liljdot <jehuoki@gmail.com>
2025-09-06 20:01:16 +0100

    add props to sidebar for conversationfield toggle in mobile view

--- client/src/components/home/Sidebar.tsx
+++ client/src/components/home/Sidebar.tsx
@@ -2,15 +2,25 @@ import LogoutButton from "@/features/auth/components/LogoutButton"
 import ConversationSearch from "@/features/conversation/components/ConversationSearch"
 import ConversationsList from "@/features/conversation/components/ConversationsList"
 
-const Sidebar = () => {
-    const conversationFieldOpen = true
+interface Props {
+    conversationFieldOpen: boolean
+    setConversationFieldOpen: (value: boolean) => void
+}
+
+const Sidebar: React.FC<Props> = ({
+    conversationFieldOpen,
+    setConversationFieldOpen
+}) => {
 
     return (
         <div className={`border-r border-slate-500 p-4 flex flex-col ${conversationFieldOpen ? "max-w-0 px-0 overflow-y-hidden" : "max-w-screen"} md:p-4 md:max-w-100 transition-width duration-500 ease-in-out`}>
             <ConversationSearch />
             <div className="divider px-3"></div>
             <ConversationsList conversations={[]} />
-            <LogoutButton />
+            <div className="mt-auto">
+                <button onClick={() => setConversationFieldOpen(!conversationFieldOpen)}>toggle</button>
+                <LogoutButton />
+            </div>
         </div>
     )
 }
