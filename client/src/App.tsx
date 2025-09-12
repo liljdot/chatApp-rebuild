7f35823bc41499b930be6167f5afd0e58a4e644c
liljdot <jehuoki@gmail.com>
2025-09-12 19:47:10 +0100

    add requireauth wrapper to home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import RequireAuth from "./features/auth/components/RequireAuth"
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
     <>
       <div className="p-4 h-screen flex items-center justify-center">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
