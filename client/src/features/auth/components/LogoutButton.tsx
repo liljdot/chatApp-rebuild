fd2d8355976c12ae996827012c446243d354efc6
liljdot <jehuoki@gmail.com>
2025-09-13 20:35:53 +0100

    implement logout functionality in logout button

--- client/src/features/auth/components/LogoutButton.tsx
+++ client/src/features/auth/components/LogoutButton.tsx
@@ -1,9 +1,12 @@
+import { useLogoutMutation } from "@/services/api/authApi"
 import { BiLogOut } from "react-icons/bi"
 
 const LogoutButton = () => {
+    const [logout] = useLogoutMutation()
+
     return (
         <div className="mt-auto">
-            <BiLogOut className="size-6 text-white cursor-pointer" />
+            <BiLogOut className="size-6 text-white cursor-pointer" onClick={() => logout(undefined)} />
         </div>
     )
 }
