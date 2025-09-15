dd69c8a333798ff07082be7a2a279a1e4fe8807a
liljdot <jehuoki@gmail.com>
2025-09-15 17:53:35 +0100

    update response type form get conversations endpoint

--- client/src/services/api/conversationsApi.ts
+++ client/src/services/api/conversationsApi.ts
@@ -1,13 +1,19 @@
 import type { ConversationForList } from "@/features/conversation/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+interface GetConversationsResponseType {
+    status: number
+    message: string
+    data: ConversationForList[]
+}
+
 export const conversationsApi = createApi({
     reducerPath: "api/conversationsApi",
     baseQuery: fetchBaseQuery({
         baseUrl: "/api/messages/",
     }),
     endpoints: builder => ({
-        getConversations: builder.query<ConversationForList[], undefined>({
+        getConversations: builder.query<GetConversationsResponseType, undefined>({
             query: () => ({
                 url: "conversations"
             })
