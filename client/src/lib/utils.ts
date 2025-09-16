e1d3b3b5b845cde92bb2ec9e7be56aa55b798ea0
liljdot <jehuoki@gmail.com>
2025-09-16 20:07:36 +0100

    add getTime utility function for formatted time output

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTime = (datestring: string) => {
+  const date = new Date(datestring)
+  const hours = padZero(date.getHours())
+  const minutes = padZero(date.getMinutes())
+  return `${hours}:${minutes}`
+}
+
+// Helper function to pad single digit numbers with leading zero
+const padZero = (num: number) => num.toString().padStart(2, '0')
