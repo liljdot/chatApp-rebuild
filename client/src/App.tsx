b08cb2d4c9adef03baf53de003458d7cc2f5396c
liljdot <jehuoki@gmail.com>
2025-09-17 19:56:54 +0100

    feat: add Toaster component for improved toast notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import RequireAuth from "./features/auth/components/RequireAuth"
+import { Toaster } from "react-hot-toast"
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+        <Toaster />
       </div>
     </>
   )
