a495035c2139d867aa34e6f104f4832b8c2b292d
liljdot <jehuoki@gmail.com>
2025-09-19 20:35:42 +0100

    feat: refactor server initialization to use socket server instance

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,7 @@ import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-
-const app = express()
+import { app, server } from "./socket/index.js"
 
 app.use(express.json()) // Middleware to parse JSON bodies
 app.use(cookieParser()) // Middleware to parse cookies
@@ -12,6 +11,6 @@ app.use(cookieParser()) // Middleware to parse cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
