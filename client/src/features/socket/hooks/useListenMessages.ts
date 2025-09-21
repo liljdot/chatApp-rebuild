787409f0b1ed5cbc0e03ff4a2d7d5fb9affe0571
liljdot <jehuoki@gmail.com>
2025-09-21 22:49:53 +0100

    feat: implement useListenMessages hook to handle incoming messages via socket

---
+++ client/src/features/socket/hooks/useListenMessages.ts
@@ -0,0 +1,23 @@
+import { useSocketContext } from "@/context/SocketContext"
+import type { MessageForConversation } from "@/features/conversation/types"
+import { addNewConversationMessage } from "@/state/slices/conversationsSlice"
+import { useAppDispatch } from "@/state/store"
+import { useEffect } from "react"
+
+const useListenMessages = () => {
+    const { socket } = useSocketContext()
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        if (socket) {
+            socket.on("newMessage", (message: MessageForConversation) => {
+                dispatch(addNewConversationMessage(message))
+            })
+        }
+    }, [
+        socket,
+        dispatch
+    ])
+}
+
+export default useListenMessages
