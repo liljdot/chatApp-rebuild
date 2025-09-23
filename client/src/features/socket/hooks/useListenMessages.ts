8699cbf8044234f82d0ddb48fe1ae0c82267d24a
liljdot <jehuoki@gmail.com>
2025-09-23 20:12:55 +0100

    add and implement notification sound on new message
    add cleanup function for newMessage listener

--- client/src/features/socket/hooks/useListenMessages.ts
+++ client/src/features/socket/hooks/useListenMessages.ts
@@ -3,6 +3,7 @@ import type { MessageForConversation } from "@/features/conversation/types"
 import { addNewConversationMessage } from "@/state/slices/conversationsSlice"
 import { useAppDispatch } from "@/state/store"
 import { useEffect } from "react"
+import notificationSound from "@/assets/sounds/notification.mp3"
 
 const useListenMessages = () => {
     const { socket } = useSocketContext()
@@ -12,8 +13,14 @@ const useListenMessages = () => {
         if (socket) {
             socket.on("newMessage", (message: MessageForConversation) => {
                 dispatch(addNewConversationMessage({ ...message, isUnread: true }))
+                const sound = new Audio(notificationSound)
+                sound.play()
             })
+
+            return () => socket.off("newMessage")
         }
+
+        return () => { }
     }, [
         socket,
         dispatch
