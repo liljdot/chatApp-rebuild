8ec5b1e355c8242a9b58357652ada1df18d94de2
liljdot <jehuoki@gmail.com>
2025-09-25 21:06:52 +0100

    allow all cors origins on socket server

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -8,7 +8,11 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: (origin, callback) => {
+            // Allow all origins (dynamically set the origin)
+            callback(null, origin || "*");
+        },
+        credentials: true,
         methods: ["GET", "POST"]
     }
 })
