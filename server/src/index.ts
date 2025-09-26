3331699ad05e69cb0fa5c73b14498918f57e3373
liljdot <jehuoki@gmail.com>
2025-09-26 19:58:15 +0100

    use usersroute in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,14 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from "./socket/index.js"
+import userRoutes from "./routes/user.route.js"
 
 app.use(express.json()) // Middleware to parse JSON bodies
 app.use(cookieParser()) // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000")
