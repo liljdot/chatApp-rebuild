e1a50ddb4630bb37840e4f0da47ce9c3465c9b36
liljdot <jehuoki@gmail.com>
2025-09-26 19:59:41 +0100

    add usersapi and middleware to store

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -5,18 +5,20 @@ import { authApi } from "@/services/api/authApi"
 import { conversationSliceReducer } from "./slices/conversationsSlice"
 import { conversationsApi } from "@/services/api/conversationsApi"
 import listenerMiddleware from "@/services/middleware/listenerMiddleware"
+import { usersApi } from "@/services/api/usersApi"
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
         conversations: conversationSliceReducer,
         [authApi.reducerPath]: authApi.reducer,
-        [conversationsApi.reducerPath]: conversationsApi.reducer
+        [conversationsApi.reducerPath]: conversationsApi.reducer,
+        [usersApi.reducerPath]: usersApi.reducer
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware()
             .prepend(listenerMiddleware.middleware)
-            .concat(authApi.middleware, conversationsApi.middleware)
+            .concat(authApi.middleware, conversationsApi.middleware, usersApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
