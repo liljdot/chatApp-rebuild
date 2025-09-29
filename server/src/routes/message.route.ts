7ef295fd98d463fae6f6515fee3523dbf86ecd20
liljdot <jehuoki@gmail.com>
2025-09-29 19:24:08 +0100

    create getconversationbyuserid endpoint in messageroutes and use controller

--- server/src/routes/message.route.ts
+++ server/src/routes/message.route.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import requireAuth from "../middleware/requireAuth.js";
-import { getConversationMessages, getConversations, postSendMessage } from "../controllers/message.controller.js";
+import { getConversationByUserId, getConversationMessages, getConversations, postSendMessage } from "../controllers/message.controller.js";
 
 const messageRoutes = express()
 
 messageRoutes.get("/conversations", requireAuth, getConversations)
+messageRoutes.get("/conversations/byUserId/:targetUserId", requireAuth, getConversationByUserId)
 messageRoutes.get("/:conversationId", requireAuth, getConversationMessages)
 messageRoutes.post("/send/:recipientId", requireAuth, postSendMessage)
 
