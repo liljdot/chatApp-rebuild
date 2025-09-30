b0b94466edf87b1b34673004ba423040935c85f5
liljdot <jehuoki@gmail.com>
2025-09-30 19:04:54 +0100

    remove selected conversation while setting selected user and set conversation field open on user avater click

--- client/src/features/users/components/UserAvatar.tsx
+++ client/src/features/users/components/UserAvatar.tsx
@@ -1,8 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui"
+import { useConversationFieldContext } from "@/context/ConversationFieldContext"
 import { useSocketContext } from "@/context/SocketContext"
 import type { User } from "@/features/auth/types"
+import { setSelectedConversation } from "@/state/slices/conversationsSlice"
 import { setSelectedUser } from "@/state/slices/usersSlice"
 import { useAppDispatch } from "@/state/store"
+import type { EventHandler, SyntheticEvent } from "react"
 
 interface Props {
     user: User
@@ -11,14 +14,22 @@ interface Props {
 const UserAvatar: React.FC<Props> = ({ user }) => {
     const dispatch = useAppDispatch()
 
+    const { setConversationFieldOpen } = useConversationFieldContext()
+
     const { onlineUserIds } = useSocketContext()
     const userIsOnline = onlineUserIds.includes(user.id)
+
+    const handleClick: EventHandler<SyntheticEvent> = () => {
+        dispatch(setSelectedUser(user))
+        dispatch(setSelectedConversation(null))
+        setConversationFieldOpen(true)
+    }
 
     return (
         <div
             className="flex flex-col w-16 text-center justify-start items-center gap-1 shrink-0 hover:bg-gray-600/50 cursor-pointer"
             key={user.id}
-            onClick={() => dispatch(setSelectedUser(user))}
+            onClick={handleClick}
         >
             <Avatar online={userIsOnline}>
                 <AvatarImage className="rounded-full" src={user.profilePic}>
