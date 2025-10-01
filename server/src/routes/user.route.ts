53f61665d4982d0df780efb800fa4c1dab8a3e4e
liljdot <jehuoki@gmail.com>
2025-10-01 18:16:49 +0100

    add getUser endpoint with getUser controller in userROutes

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUsers } from "../controllers/user.controller.js"
+import { getAllUsers, getUser } from "../controllers/user.controller.js"
 import requireAuth from "../middleware/requireAuth.js"
 
 const userRoutes = express()
 
 userRoutes.get("/", requireAuth, getAllUsers)
+userRoutes.get("/:id", requireAuth, getUser)
 
 export default userRoutes
