b37fd4aceec9f70ffe602cdd19493dca284e9ed2
liljdot <jehuoki@gmail.com>
2025-10-01 18:16:01 +0100

    create get single user controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,6 +2,58 @@ import { Request, Response } from "express";
 import prisma from "../db/prisma.js";
 import { CleanUser } from "../types/index.js";
 
+const getUser = (req: Request, res: Response) => {
+    const { error, user, params: { id } } = req
+
+    if (error) {
+        return res.status(error.status || 500).json({
+            status: error.status || 500,
+            message: error.message || "Something went wrong",
+            error: error.error || "Internal server error"
+        })
+    }
+
+    if (!user) {
+        return res.status(401).json({
+            status: 401,
+            message: "You must be logged in to view conversations",
+            error: "Unauthorized"
+        })
+    }
+
+    prisma.user.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            username: true,
+            createdAt: true,
+            profilePic: true,
+            fullName: true,
+            gender: true,
+            updatedAt: true
+        }
+    })
+    .then(user => !user 
+        ? Promise.reject({
+            status: 404,
+            message: "User not found",
+            error: "Not Found"
+        })
+        : res.status(200).json({
+            status: 200,
+            message: "Get user was successful",
+            data: user
+        })
+    )
+    .catch(err => res.status(err.status || 500).json({
+        status: err.status || 500,
+        message: err.message || "Something went wrong",
+        error: err.error || "Internal server error"
+    }))
+}
+
 const getAllUsers = (req: Request, res: Response) => {
     const { error, user } = req
 
@@ -60,5 +112,6 @@ const getAllUsers = (req: Request, res: Response) => {
 }
 
 export {
+    getUser,
     getAllUsers
 }
