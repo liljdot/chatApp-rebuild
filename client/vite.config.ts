e9fb01339f1c97071aab7b552934ece3b62a80c2
liljdot <jehuoki@gmail.com>
2025-10-01 22:03:48 +0100

    redo vite aliasing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react"
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 import os from "os"
 
 function getLocalExternalIp() {
@@ -21,6 +22,11 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
   server: {
     proxy: {
       "/api": {
