6c978ffa92116b7f2dbc9fc7570edf6ee0c38264
liljdot <jehuoki@gmail.com>
2025-10-01 19:59:06 +0100

    deployment config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from "./socket/index.js"
 import userRoutes from "./routes/user.route.js"
+
+const __dirname = path.resolve()
 
 app.use(express.json()) // Middleware to parse JSON bodies
 app.use(cookieParser()) // Middleware to parse cookies
@@ -12,6 +15,13 @@ app.use(cookieParser()) // Middleware to parse cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+if (process.env.NODE_ENV != "development") {
+    app.use(express.static(path.join(__dirname, "/client/dist")))
+    app.get("/{*any}", (req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+    })
+}
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000")
