995ee0d41a78eb6a886f59a6b7bd7383675ed437
liljdot <jehuoki@gmail.com>
2025-10-01 19:56:05 +0100

    setup hom display

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import Sidebar from "@/components/home/Sidebar"
+import ConversationField from "@/features/conversation/components/ConversationField"
+import useListenMessages from "@/features/socket/hooks/useListenMessages"
+import type { RootState } from "@/state/store"
+import { useSelector } from "react-redux"
 
 const Home = () => {
+  const { selectedConversationId } = useSelector((state: RootState) => state.conversations)
+  const { selectedUser } = useSelector((state: RootState) => state.users)
+  useListenMessages()
+
   return (
-    <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/0 bg-clip-padding backdrop-filter backdrop-blur-lg">
+    <div className="flex h-screen w-screen md:w-full md:h-[550px] rounded-lg overflow-hidden bg-gray-400/0 bg-clip-padding backdrop-filter backdrop-blur-lg">
       <Sidebar />
+      <ConversationField
+        conversationId={selectedConversationId}
+        targetUser={selectedUser}
+      />
     </div>
   )
 }
